fa5d5bb25d6f6df25879b6fe9ddca4cbfe83a34a
Steve Kimoi <moenisteve@gmail.com>
2022-11-28 13:51:50 +0300

    geometry nft contract

---
+++ contracts/Geometry.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts@4.4.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.4.0/access/Ownable.sol";
+
+
+contract Geometry is ERC721, Ownable {
+    constructor () ERC721("Geometry", "GMT") {} 
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://ipfs.io/ipfs/QmcfsZCGdcXyHJP8MupMQwYuSET8TFJCHUhAaoJwe4EJuP"; 
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId); 
+    }
+}
