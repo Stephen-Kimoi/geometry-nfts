9f0edc246b08c33952e092d9db9509395a953950
Steve Kimoi <moenisteve@gmail.com>
2022-11-29 10:11:54 +0300

    solving the openzeppelin error

--- contracts/Geometry.sol
+++ contracts/Geometry.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts@4.4.0/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.4.0/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract Geometry is ERC721, Ownable {
